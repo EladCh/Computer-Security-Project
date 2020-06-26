eec20b09ec74d5960502692d5f04777ab8f601a2
maticrown <49392006+maticrown@users.noreply.github.com>
2020-06-26 14:55:58 +0300

    Update script-centos7.sh

--- script-centos7.sh
+++ script-centos7.sh
@@ -6,3 +6,9 @@ sudo easy_install-3.6 pip
 #extra dependencies
 sudo pip3 install mmh3
 sudo pip3 install bitarray
+
+#pysimplegui
+sudo pip3 install PySimpleGUI
+
+#running the program
+sudo python3 -m gui
