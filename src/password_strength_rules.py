d21a66b5b022a54c1ec946d94747d7966052fc71
maticrown <49392006+maticrown@users.noreply.github.com>
2020-06-26 15:08:23 +0300

    Update password_strength_rules.py (#17)

--- src/password_strength_rules.py
+++ src/password_strength_rules.py
@@ -1,16 +1,18 @@
 import string
 import collections
 
+mandatory_violation = False
+
 def password_rules(password):
     # format for strengths and weaknesses. the default value is False.
-    weaknesses = [['All letters:', 'True'], #0
-                  ['All digits','True'], #1
-                  ['Repeated characters:', 'True'], #2
-                  ['Consecutive uppercase letters','True'], #3
-                  ['Consecutive lowercase letters:', 'True'], #4
-                  ['Consecutive digits','True'], #5
-                  ['Sequential letters:', 'True'], #6
-                  ['Sequential digits','True']] #7
+    weaknesses = [['All letters:', 'False'], #0
+                  ['All digits','False'], #1
+                  ['Repeated characters:', 'False'], #2
+                  ['Consecutive uppercase letters','False'], #3
+                  ['Consecutive lowercase letters:', 'False'], #4
+                  ['Consecutive digits','False'], #5
+                  ['Sequential letters:', 'False'], #6
+                  ['Sequential digits','False']] #7
 
     strengths = [['Length:', "False"], #0
                  ['Upper case letters',"False"], #1
@@ -20,23 +22,25 @@ def password_rules(password):
                  ['Digits & Symbols in the middle',"False"]] #5
 
     score = 0
-    popup_msg="Password accepted"
+    popup_msg="The password does not uphold to the standards due to:\n"
     mandatory_violation = False
 
+
     # mandatory requirements check
+
     if (len(password)>=8):
         score += (len(password)*4)
         strengths[0][1] = 'True'
     else:
-        popup_msg="Password is too short\nPlease check the analysis for correction"
+        popup_msg+="Password is too short.\n"
         mandatory_violation = True
-    
+
     if any(x.isupper()for x in password):
         num_of_upper = [x for x in password if x.isupper()]
         score += (len(num_of_upper)*2)
         strengths[1][1] = 'True'
     else:
-        popup_msg="Password contains no upper class letters\nPlease check the analysis for correction"
+        popup_msg+="Password contains no upper class letters.\n"
         mandatory_violation = True
 
     if any(x.islower()for x in password):
@@ -44,7 +48,7 @@ def password_rules(password):
         score += (len(num_of_lower)*2)
         strengths[2][1] = 'True'
     else:
-        popup_msg="Password contains no lower class letters\nPlease check the analysis for correction"
+        popup_msg+="Password contains no lower class letters.\n"
         mandatory_violation = True
 
     if any(x.isdigit()for x in password):
@@ -52,15 +56,16 @@ def password_rules(password):
         score += (len(num_of_digits)*4)
         strengths[3][1] = 'True'
     else:
-        popup_msg="Password contains no digits\nPlease check the analysis for correction"
+        popup_msg+="Password contains no digits.\n"
         mandatory_violation = True
 
+###changed to check for special character
     if any(x in string.punctuation for x in password):
         num_of_signs = [x for x in password if x in string.punctuation]
         score += (len(num_of_signs)*6)
         strengths[4][1] = 'True'
     else:
-        popup_msg="Password contains no special characters\nPlease check the analysis for correction"
+        popup_msg+="Password contains no special characters.\n"
         mandatory_violation = True
 
     if len(password)>2:
@@ -71,40 +76,82 @@ def password_rules(password):
             score += (len(num_of_signs)*2)
             strengths[5][1] = 'True'
 
-    # TODO penalties reduction
-    # checking if string is all letters
+
+## checking if string is all letters
     if (password.isalpha()):
         score -= (len(password)/4)
-    else:
-        weaknesses[0][1] = False
+        popup_msg+="Password is all letters.\n"
+        mandatory_violation=True
 
-    # checking if string is all digits
+## checking if string is all digits
     if(password.isdigit()):
         score -= (len(password)/4)
-    else:
-        weaknesses[1][1] = False
+        popup_msg+="Password is all digits.\n"
+        mandatory_violation=True
 
-    # checking for repeated characters
-    d = collections.defaultdict(int)
-    e = 0
-    for c in password:
-        d[c] += 1 
-    for c in sorted(d, key=d.get, reverse=True):
-        if (d[c] > 1):
-            e +=d[c]
-    if e > 0:
-        e /= 2
-        score -= e*(e-1)
-    else:
-        weaknesses[2][1] = False
+### checking for repeated characters
+    results=0
+    results = collections.Counter(password)
+    for i in results: 
+        if results[i]>1:
+            score -= results[i]*(results[i]-1)
+            popup_msg+="Password has repeated characters.\n"
+            mandatory_violation=True
 
-    # checking if there are consecutive uppercase characters
-    l = len(password)
-    s = ''.join(sorted(password))
+### checking if there are consecutive uppercase characters
+    countUpper=0
+    l=len(password)
 
-    for i in range(1,l):
-        if ord(s[i]) is not ord(s[i-1]):
-            print("false")
+    for i in range(1,l-1):
+        if (password[i] == password[i+1] and password[i].isupper()):
+            countUpper += 1
+        else:
+            countUpper=1
+
+        if countUpper>1:
+            score -= countUpper*2
+            popup_msg+="Password has consecutive uppercase letters.\n"
+            mandatory_violation=True
+
+### checking if there are consecutive lowercase characters
+    countLower=0
+    for i in range(1,l-1):
+        if(password[i] == password[i-1] and password[i].islower()):
+            countLower += 1
+        else:
+            counterLower=1
+        if countLower>1:
+            score -= countLower*2
+            popup_msg+="Password has consecutive lowercase letters.\n"
+            mandatory_violation=True
+
+##checking if there are sequential letters
+    sequentialLettersCountUp=0
+    sequentialLettersCountDown=0
+
+    for letter in password:
+        if ord(letter)<ord(letter)+1 and letter.isalpha():
+            sequentialLettersCountUp+=1
+        if ord(letter)>ord(letter)+1 and letter.isalpha():
+            sequentialLettersCountDown+=1
+    if sequentialLettersCountUp>=4 or sequentialLettersCountDown>=4:
+        score -= (sequentialLettersCountUp+sequentialLettersCountDown)
+        popup_msg+="Password has sequential letters.\n"
+        mandatory_violation=True
+
+##checking if there are sequential digits
+    sequentialDigitsCountUp=0
+    sequentialDigitsCountDown=0
+
+    for digit in password:
+        if ord(digit)<ord(digit)+1 and digit.isdigit():
+            sequentialDigitsCountUp+=1
+        if ord(digit)>ord(digit)+1 and digit.isdigit():
+            sequentialDigitssCountDown+=1
+    if sequentialDigitsCountUp>=4 or sequentialDigitsCountDown>=4:
+        score -= (sequentialDigitsCountUp+sequentialDigitsCountDown)
+        popup_msg+="Password has sequential digits.\n"
+        mandatory_violation=True
 
     # set the score
     if score < 20:
@@ -117,4 +164,5 @@ def password_rules(password):
         score_txt = 'Strong'
     elif score >= 80:
         score_txt = 'Very Strong'
-    return weaknesses, strengths, score_txt, popup_msg, mandatory_violation
+
+    return  weaknesses, strengths, score_txt, popup_msg, mandatory_violation
